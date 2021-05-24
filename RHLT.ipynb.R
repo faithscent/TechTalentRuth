aa44db814197d15ea921b5fa9908e87f257f4b97
faithscent <81152018+faithscent@users.noreply.github.com>
2021-05-24 18:33:10 +0100

    R homework 1

---
+++ RHLT.ipynb.R
@@ -0,0 +1,24 @@
+Print <- "hello world"
+
+#task1
+
+my_name <- readline("Enter name: ")
+my_age <- readline("Enter age: ")
+hello <-print(paste("Hi," ,"My name is", my_name, "and I am ", my_age, "years old today"))
+
+#task2 
+
+food_types <- list("fruits", "legumes", "vegetables", "starch", "protein")
+ls()
+
+#task 3
+print(seq(20:50))
+print(mean(20:60))
+print(sum(51:91))
+
+# task 4 
+samplenum <- sample(-50:+50, 10)
+print(samplenum)
+
+
+
